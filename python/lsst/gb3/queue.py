5bb1a61819587e5e24f84a1e3deae858265816ac
price <price@git.lsstcorp.org>
2010-11-06 19:15:25 +0000

    Add functionality to queue (with PBS, etc.) multiple reductions.

---
+++ python/lsst/gb3/queue.py
@@ -0,0 +1,45 @@
+#/usr/bin/env python
+
+import pickle
+import subprocess
+import shlex
+
+class Queue(object):
+    def __init__(self, script, importList=None, command="qsub -V", resourceList=None, queue=None):
+        self.script = script
+        self.importList = importList
+        self.command = command
+        self.resourceList = resourceList
+        self.queue = queue
+        return
+
+    def sub(self, name, **kwargs):
+        filename = name + ".py"
+        fp = open(filename, 'w')
+        fp.write("#/usr/bin/env python\n")
+        fp.write("import pickle\n")
+        for imp in self.importList:
+            if not isinstance(imp, str) and getattr(imp, '__iter__', None) is not None:
+                if len(imp) == 1:
+                    imp = imp[0]
+                elif len(imp) == 2:
+                    imp = "%s as %s" % (imp[0], imp[1])
+                else:
+                    raise RuntimeError("Can't understand attempted import: %s" % imp)
+            fp.write("import %s\n" % imp)
+        for key, val in kwargs.items():
+            fp.write("%s = pickle.loads(\"\"\"%s\n\"\"\")\n" % (key, pickle.dumps(val)))
+        fp.write(self.script + "\n")
+        fp.close()
+
+        command = self.command
+        if self.resourceList is not None:
+            if isinstance(self.resourceList, str):
+                command += " -l " + self.resourceList
+            else:
+                command += " -l " + ','.join(self.resourceList)
+        if self.queue is not None:
+            command += " -q %s" % self.queue
+        command += " " + filename
+        print command
+        #subprocess.call(shlex.split(command))

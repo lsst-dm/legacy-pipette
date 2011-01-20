203bf19ce56c14fdf152d3927117dbe776d28517
price <price@git.lsstcorp.org>
2011-01-20 21:16:39 +0000

    Redesign to make more procedural and configurable.

--- python/lsst/pipette/ccd.py
+++ python/lsst/pipette/ccd.py
@@ -6,8 +6,18 @@ import lsst.pipette.bootstrap as bsProc
 import lsst.pipette.char as charProc
 
 class Ccd(pipProc.Process):
-    def __init__(self, Isr=None):
-        self.Isr = isrProc.isr
+    def __init__(self, isr=isrProc.Isr, bootstrap=bsProc.Bootstrap, char=charProc.Char, *args, **kwargs):
+        """Initialise
+
+        @param isr Process to do ISR
+        @param bootstrap Process to do Bootstrap
+        @param char Process to do Char
+        """
+        super(Ccd, self).__init__(*args, **kwargs)
+        self._isr = isr
+        self._bootstrap = bootstrap
+        self._char = char
+        
         
     def run(self, exposureList, detrendsList=None):
         """Process a CCD.
@@ -18,18 +28,24 @@ class Ccd(pipProc.Process):
         """
         assert exposureList and len(exposureList) > 0, "exposureList not provided"
 
-        exposure = self.isr(exposureList, detrends)
+        self.isr(exposureList, detrendsList)
 
-        exposure, defects, psf, apcorr = bsProc.Bootstrap(config=self.config, log=self.log).run(exposureList)
+        exposure, defects, psf, apcorr = self.bootstrap(exposureList)
 
-        sources, matches = charProc.Char(config=self.config, log=self.log).run(exposure, psf, apcorr, defects)
+        sources, matches = self.char(exposure, psf, apcorr, defects)
 
         return exposure, psf, apcorr, sources, matches
             
-    def isr(self, exposureList, detrends):
+    def isr(self, exposureList, detrendsList):
+        isr = self._isr(config=self.config, log=self.log)
         for index, exp in enumerate(exposureList):
             detrends = detrendsList[index] if detrendsList else None
-            self.isr(exp, detrends)
+            isr.run(exp, detrends)
 
-        isr = self.Isr(config=self.config, log=self.log)
-        return isr.run(exp, detrends)
+    def bootstrap(self, exposureList):
+        bootstrap = self._bootstrap(config=self.config, log=self.log)
+        return bootstrap.run(exposureList)
+
+    def char(self, exposure, psf, apcorr, defects):
+        char = self._char(config=self.config, log=self.log)
+        return char.run(exposure, psf, apcorr, defects)

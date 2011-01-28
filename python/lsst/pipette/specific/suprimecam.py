5c423f55b8f5299ce97ab84eb5ce237f2526a417
price <price@git.lsstcorp.org>
2011-01-28 21:02:39 +0000

    Adding Suprime-Cam-specific processing: mask out the guider left by the autoguider.

---
+++ python/lsst/pipette/specific/suprimecam.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import lsst.pipette.isr as pipIsr
+import lsst.pipette.util as pipUtil
+
+class IsrSuprimecam(pipIsr.Isr):
+    def run(self, exposure, *args, **kwargs):
+        self.guider(exposure)
+        super(IsrSuprimeCam, self).run(exposure, *args, **kwargs)
+
+    def guider(self, exposure):
+        """Mask guider shadow
+
+        @param exposure Exposure to process
+        """
+        assert exposure, "No exposure provided"
+
+        ccd = pipUtil.getCcd(exposure)
+        ccdNum = ccd.getId().getSerial()
+        if ccdNum not in [0, 1, 2, 6, 7]:
+            # No need to mask
+            return
+
+        md = exposure.getMetadata()
+        if not md.has_key("S_AG_X"):
+            self.log.log(self.log.WARN, "No autoguider position in exposure metadata.")
+            return
+
+        xGuider = md["S_AG_X"]
+        if ccdNum in [1, 2, 7]:
+            maskLimit = int(60.0 * xGuider - 2300.0) # From SDFRED
+        elif ccdNum in [0, 6]:
+            maskLimit = int(60.0 * xGuider - 2000.0) # From SDFRED
+
+        
+        mi = exposure.getMaskedImage()
+        height = mi.getHeight()
+        if height < maskLimit:
+            # Nothing to mask!
+            return
+
+        self.log.log(self.log.INFO, "Masking autoguider shadow at y > %d" % maskLimit)
+        mask = mi.getMask()
+        bbox = afwImage.BBox(afwImage.PointI(0, 0), afwImage.PointI(-1, maskLimit - height))
+        badMask = mask.Factory(mask, bbox)
+
+        mask.addMaskPlane("GUIDER")
+        badBitmask = mask.getPlaneBitMask("GUIDER")
+
+        badMask |= badBitmask
+        return

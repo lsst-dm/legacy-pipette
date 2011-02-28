1d05ad3d527dcfed5a6a566254c4dccb1e47bc1c
price <price@git.lsstcorp.org>
2011-02-28 04:39:44 +0000

    Perform statistics on histogram, and plot Gaussian based on those statistics.

--- python/lsst/pipette/plotter.py
+++ python/lsst/pipette/plotter.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 
+import math
 import matplotlib
 matplotlib.use('pdf')
 import matplotlib.backends.backend_pdf
 import matplotlib.pyplot as plot
 import numpy
+import numpy.ma as ma
 
+import lsst.afw.math as afwMath
 
 def gaussian(param, x):
     norm = param[0]
     offset = param[1]
     width = param[2]
-    return norm * numpy.exp(-((x - offset)/width)**2)    
+    return norm * numpy.exp(-0.5*((x - offset)/width)**2)    
 
 class Plotter(object):
     def __init__(self, name):
@@ -33,14 +36,30 @@ class Plotter(object):
         self.pdf.savefig()
         plot.close()
 
-    def histogram(self, data, range, bins=51, mean=None, sigma=None, title=None):
+    def histogram(self, data, bounds, bins=51, gaussFit=True, iterations=3, clip=3.0, title=None):
         plot.figure()
-        n, bins, patches = plot.hist(data, bins=bins, range=range, normed=False,
+        n, bins, patches = plot.hist(data, bins=bins, range=bounds, normed=False,
                                      histtype='bar', align='mid')
-        norm = n.max()
-        if mean is not None and sigma is not None:
-            gauss = gaussian([norm, mean, sigma], bins)
-            plot.plot(bins, gauss, 'r-')
+
+        if gaussFit:
+            calc = afwMath.vectorF()
+            for value in data:
+                if value > bounds[0] and value < bounds[1]:
+                    calc.push_back(value)
+            stats = afwMath.makeStatistics(calc, afwMath.MEANCLIP | afwMath.STDEVCLIP)
+            mean = stats.getValue(afwMath.MEANCLIP)
+            stdev = stats.getValue(afwMath.STDEVCLIP)
+            num = len(calc)
+
+            print "Histogram statistics:", num, mean, stdev
+            norm = num * (bins[1:] - bins[:-1]).mean() / math.sqrt(2.0 * math.pi) / stdev
+            middle = (bins[:-1] + bins[1:]) / 2.0
+            gauss = gaussian([norm, mean, stdev], middle)
+            plot.plot(middle, gauss, 'r-', label='%d*N(%.3f, %.3f^2)' % (num, mean, stdev))
+            leg = plot.legend(loc='upper right')
+            for t in leg.get_texts():
+                t.set_fontsize('small')
+
         if title is not None:
             plot.title(title)
         self.pdf.savefig()

6e9d76278e01a21f3ceb1e06a57059626e1e6ba4
price <price@git.lsstcorp.org>
2011-03-03 15:12:21 +0000

    Add filter input, and fix up CCD list.

--- bin/hsc_stack.py
+++ bin/hsc_stack.py
@@ -14,6 +14,7 @@ def run(rerun,                          # Rerun name
         ccds,                           # CCD number
         stack,                          # Stack identifier
         patch,                          # Patch identifier
+        filter,                         # Filter name
         config,                         # Configuration
         coords,                         # Skycell centre coordinates
         scale,                          # Pixel scale
@@ -45,12 +46,14 @@ if __name__ == "__main__":
                       help="rerun name (default=%default)")
     parser.add_option("-f", "--frames", dest="frames",
                       help="visit to run, colon-delimited")
-    parser.add_option("-c", "--ccds", dest="ccds", default="0:1:2:3:4:5:6:7:8:9",
+    parser.add_option("-c", "--ccds", dest="ccds", default="0:1:2:3:4:5:6:7:8:9:10:11:12:13:14:15:16:17:18:19:20:21:22:23:24:25:26:27:28:29:30:31:32:33:34:35:36:37:38:39:40:41:42:43:44:45:46:47:48:49:50:51:52:53:54:55:56:57:58:59:60:61:62:63:64:65:66:67:68:69:70:71:72:73:74:75:76:77:78:79:80:81:82:83:84:85:86:87:88:89:90:91:92:93:94:95:96:97:98:99",
                       help="CCD to run (default=%default)")
     parser.add_option("-s", "--stack", dest="stack", type="int",
                       help="Stack identifier")
     parser.add_option("-p", "--patch", dest="patch", type="int",
                       help="Patch identifier")
+    parser.add_option("--filter", dest="filter", type="string",
+                      help="Stack identifier")
     parser.add_option("--coords", dest="coords", type="float", nargs=2,
                       help="Coordinates for skycell, degrees")
     parser.add_option("--scale", dest="scale", type="float",
@@ -68,4 +71,4 @@ if __name__ == "__main__":
         sys.exit(1)
 
     run(opts.rerun, map(int, opts.frames.split(":")), map(int, opts.ccds.split(":")), opts.stack, opts.patch,
-        config, opts.coords, opts.scale, opts.sizes)
+        opts.filter, config, opts.coords, opts.scale, opts.sizes)

8573e2bebb0b053f1343d7e8a9d8072aa6ae7440
price <price@git.lsstcorp.org>
2011-07-20 15:28:59 +0000

    Adding first cut at timer using function decoration.

---
+++ python/lsst/pipette/timer.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+
+import time
+
+"""Timing decorator, for measuring execution time.
+
+This is the "timecall" part of "profilehooks" from
+http://mg.pov.lt/profilehooks/svn/trunk/profilehooks.py,
+with some extensions to enable run-time configuration.
+
+The license for the original code, profilehooks, is:
+
+  Copyright (c) 2004--2008 Marius Gedminas <marius@pov.lt>
+  Copyright (c) 2007 Hanno Schlichting
+  Copyright (c) 2008 Florian Schulze
+
+  Released under the MIT licence since December 2006:
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+"""
+
+
+
+def timecall(fn=None, active=False, immediate=True, report=True):
+    """Wrap `fn` and print its execution time.
+
+    Example::
+
+        @timecall(active=True)
+        def somefunc(x, y):
+            time.sleep(x * y)
+
+        somefunc(2, 3)
+
+    will print the time taken by somefunc on every call, and a report at
+    program termination.  By default (no options passed to decorator @timecall),
+    timing is OFF, but can be turned on using TimerConfig:
+
+        TimerConfig.setActive(True)
+
+    Additional run-time configuration is available using the TimerConfig singleton
+    class (immediate reports for all decorated functions, different timer).
+    """
+    if fn is None: # @timecall() syntax -- we are a decorator maker
+        def decorator(fn):
+            return timecall(fn, immediate=immediate, report=report)
+        return decorator
+    # @timecall syntax -- we are a decorator.
+    fp = FuncTimer(fn, immediate=immediate, report=report)
+    # We cannot return fp or fp.__call__ directly as that would break method
+    # definitions, instead we need to return a plain function.
+    def new_fn(*args, **kw):
+        return fp(*args, **kw)
+    new_fn.__doc__ = fn.__doc__
+    new_fn.__name__ = fn.__name__
+    new_fn.__dict__ = fn.__dict__
+    new_fn.__module__ = fn.__module__
+    return new_fn
+
+
+
+class TimerConfig(object):
+    """Singleton class providing global run-time configuration of function timing."""
+
+    # Singletonisation
+    # From http://stackoverflow.com/questions/42558/python-and-the-singleton-pattern/1810367#1810367
+    _instance = None
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            cls._instance = super(TimerConfig, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    def __init__(self)
+        self.active = False
+        self.immediate = False
+        self.timer = time.time
+
+    @classmethod
+    def setActive(cls, active):
+        self = cls()
+        active, self.active = self.active, active
+        return active
+
+    @classmethod
+    def getActive(cls):
+        return cls().active
+
+    @classmethod
+    def setImmediate(cls, immediate):
+        self = cls()
+        immediate, self.immediate = self.immediate, immediate
+        return immediate
+
+    @classmethod
+    def getImmediate(cls):
+        return cls().immediate
+
+    @classmethod
+    def setTimer(cls, timer):
+        self = cls()
+        timer, self.timer = self.timer, timer
+        return timer
+
+    @classmethod
+    def getTimer(cls):
+        return cls().timer
+
+
+class FuncTimer(object):
+    def __init__(self, fn, immediate, report):
+        self.fn = fn
+        self.ncalls = 0
+        self.totaltime = 0
+        self.immediate = immediate
+        if report:
+            atexit.register(self.atexit)
+
+    def __call__(self, *args, **kw):
+        """Profile a singe call to the function."""
+        fn = self.fn
+
+        config = TimerConfig()
+        if not config.getActive():
+            return fn(*args, **kw)
+
+        timer = config.getTimer()
+        self.ncalls += 1
+        try:
+            start = timer()
+            return fn(*args, **kw)
+        finally:
+            duration = timer() - start
+            self.totaltime += duration
+            if self.immediate or config.getImmediate():
+                funcname = fn.__name__
+                filename = fn.func_code.co_filename
+                lineno = fn.func_code.co_firstlineno
+                print >> sys.stderr, "\n  %s (%s:%s):\n    %f seconds\n" % (
+                                        funcname, filename, lineno, duration)
+    def atexit(self):
+        if not self.ncalls:
+            return
+        funcname = self.fn.__name__
+        filename = self.fn.func_code.co_filename
+        lineno = self.fn.func_code.co_firstlineno
+        print ("\n  %s (%s:%s):\n"
+               "    %d calls, %.3f seconds (%.3f seconds per call)\n" % (
+                                funcname, filename, lineno, self.ncalls,
+                                self.totaltime, self.totaltime / self.ncalls))
+
